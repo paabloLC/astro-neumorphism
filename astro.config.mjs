f3e45512e4d12e1de41bce3c683f3d6b63f5e0f5
Pablo Lara Cerero <pablolc@pabloLC.technicolor.net>
2022-05-29 22:10:55 +0200

    fix and change base.css file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ export default defineConfig({
 	integrations: [tailwind({
 		// Example: Disable injecting a basic `base.css` import on every page.
     // Useful if you need to define and/or import your own custom `base.css`.
-    // config: { applyBaseStyles: false },
+    config: { applyBaseStyles: true },
 	})],
 });
